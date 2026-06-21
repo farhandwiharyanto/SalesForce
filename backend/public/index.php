420be5894acd359329bf9b481ced21f32e3f8562
farhandwiharyanto <farhandwiharyanto30@gmail.com>
2026-06-22 01:21:37 +0700

    chore: remove debug code

--- backend/public/index.php
+++ backend/public/index.php
@@ -20,8 +20,6 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 try {
     // Vercel deployment fix: Force SCRIPT_NAME to /index.php so Laravel doesn't strip the /api prefix
     $_SERVER['SCRIPT_NAME'] = '/index.php';
-    $_ENV['APP_DEBUG'] = 'true';
-    putenv('APP_DEBUG=true');
     
     $app->handleRequest(Request::capture());
 } catch (\Throwable $e) {
